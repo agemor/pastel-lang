5fb68359c1db3654c5cfdcb7259be65d33c2cf93
HyunJun Kim <me@hyunjun.org>
2016-10-23 16:43:19 +0900

    Update Babel transpiler support

--- src/lexer.js
+++ src/lexer.js
@@ -4,97 +4,16 @@ import Tokenizer from "./tokenizer.js";
 class Lexer {
 
     constructor() {
-
+        this.tokenizer = new Tokenizer();
     }
 
     analyze(text) {
-        var tokens = this.tokenize(text);
-
+        var tokens = this.tokenizer.tokenize(text);
+        console.log(tokens);
 
     }
 
-    /**
-     * Split code by syllables
-     */
-    tokenize(text) {
-
-        // Unify whitespace format
-        text = text.replace(/\s/g, " ");
-
-        let tokens = [];
-        let buffer = "";
-
-        // buffer flush
-        function flush() {
-            if (buffer.length > 0) {
-                tokens.push(buffer);
-                buffer = "";
-            }
-        }
-
-        // Space flags
-        let spaceFlag = false;
-
-        // String flags
-        let stringOpened = false;
-        let stringOpener = "";
-
-        for (let i in text) {
-
-            let char = text.charAt(i);
-
-            // Space is delimiter
-            if (char == " " && !stringOpened) {
-                if (!spaceFlag) {
-                    spaceFlag = true;
-                    flush();
-                }
-                // Space is non-insertive
-                continue;
-            } else {
-                spaceFlag = false;
-            }
-
-            // Parens as delemiter
-            if ((char == "(" || char == ")") && !stringOpened) {
-                if (char == "(") {
-                    buffer += char;
-                    flush();
-                } else {
-                    flush();
-                    buffer += char;
-                }
-                continue;
-            }
-
-            // Bundle up strings as delimiter
-            if (char == "\"" || char == "\'") {
-
-                // String open
-                if (!stringOpened) {
-                    stringOpener = char;
-                    stringOpened = true;
-                    flush();
-                    buffer += char;
-                    continue;
-                }
-                // String close
-                else if (stringOpener == char) {
-                    stringOpened = false;
-                    buffer += char;
-                    flush();
-                    continue;
-                }
-            }
-
-            // Write buffer
-            buffer += char;
-        }
-        // Last flush
-        flush();
-
-        return syllables;
-    }
+    
 }
 
 export default Lexer;
